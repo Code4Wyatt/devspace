b681f915e83ec3dd2dc834abe1afadae0f7c2246
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-06-04 23:52:11 +0100

    Updated Style

--- devspace/src/components/EmployerLogin/EmployerLogin.jsx
+++ devspace/src/components/EmployerLogin/EmployerLogin.jsx
@@ -3,7 +3,15 @@ import './EmployerLogin.scss'
 
 const EmployerLogin = () => {
   return (
-    <div className="employer-section">EmployerLogin</div>
+    <div className="employer-container">
+          <h3>Register As An Employer</h3>
+          <form>
+              <input type="text" className="form-control" placeholder="Company Name"/>
+              <input type="text" className="form-control" placeholder="Company Contact"/>
+              <input type="text" className="form-control" placeholder="Email"/>
+              <input type="text" className="form-control" placeholder="Password"/>
+          </form>
+      </div>
   )
 }
 
