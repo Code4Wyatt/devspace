e5fc1ef8de16e91285b74c957046debb9c148412
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-06-22 00:13:51 +0100

    Updated Homepage Design
    Added MaterialUI components for responsive design, more detail to be added later.

--- devspace/src/pages/Home/Home.js
+++ devspace/src/pages/Home/Home.js
@@ -1,11 +1,11 @@
-import './Home.scss'
-import { useState, useEffect } from "react"
-import { useSelector, connect, useDispatch } from "react-redux"
-import { addCurrentUserAction } from "../../redux/actions/UserAction"
-import Nav from "../../components/Nav/Nav"
-import abstract from "../../assets/abstract.png"
-import geometric from "../../assets/geometric.png"
-import { Container } from "@material-ui/core"
+import "./Home.scss";
+import { useState, useEffect } from "react";
+import { useSelector, connect, useDispatch } from "react-redux";
+import { addCurrentUserAction } from "../../redux/actions/UserAction";
+import Nav from "../../components/Nav/Nav";
+import abstract from "../../assets/abstract.png";
+import geometric from "../../assets/geometric.png";
+import { Container, Grid, Card, Typography } from "@material-ui/core";
 
 const Home = (props) => {
   const [profile, setProfile] = useState([]);
@@ -46,20 +46,84 @@ const Home = (props) => {
 
   return (
     <>
-      <div className="main">
-        <img src={abstract} className="abstract" />
-
+      <main>
         <div className="home__banner">
-        <h1 className="home__banner-text animate-charcter2">Discover developers.</h1>
-          <h1 className="home__banner-subtitle animate-charcter">
+          {/* <img src={abstract} className="abstract" /> */}
+          <Typography
+            variant="h1"
+            component="h2"
+            className="home__banner-text animate-charcter2"
+          >
+            Discover developers.
+          </Typography>
+          <Typography
+            variant="h1"
+            component="h2"
+            className="home__banner-subtitle animate-charcter"
+          >
             Engage with employers.
-          </h1>
+          </Typography>
         </div>
-          
-      </div>
-
+        <div className="mission__statement">
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            className="mission__statement-grid"
+          >
+            <Grid item xs={6}>
+              <Typography
+                variant="h4"
+                component="h2"
+                className="mission__statement-statement"
+              >
+                Find the latest developer talent, globally.
+              </Typography>
+              {/* <Card variant="outlined" className="mission__statement-cards">
+                <p>Discover the latest and greatest graduates in multiple disciplines</p>
+              </Card> */}
+            </Grid>
+            <Grid item xs={6}>
+              <Typography
+                variant="h4"
+                component="h2"
+                className="mission__statement-statement"
+              >
+                Connect with employers, build a network.
+              </Typography>
+              {/* <Card variant="outlined" className="mission__statement-cards">
+                <p>Hi There</p>
+              </Card> */}
+            </Grid>
+            <Grid item xs={6}>
+              <Typography
+                variant="h4"
+                component="h2"
+                className="mission__statement-statement"
+              >
+                Developer portal to display projects and skills.
+              </Typography>
+              {/* <Card variant="outlined" className="mission__statement-cards">
+                <p>Hi There</p>
+              </Card> */}
+            </Grid>
+            <Grid item xs={6}>
+              <Typography
+                variant="h4"
+                component="h2"
+                className="mission__statement-statement"
+              >
+                Employer data to find the most suitable talent.
+              </Typography>
+              {/* <Card variant="outlined" className="mission__statement-cards">
+                <p>Hi There</p>
+              </Card> */}
+            </Grid>
+          </Grid>
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
