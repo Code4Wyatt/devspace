e165c8cb9256468945cd2ba4138074a2fb6fb694
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-06-24 00:57:47 +0100

    Added Employer Card component

--- devspace/src/pages/Employers/Employers.js
+++ devspace/src/pages/Employers/Employers.js
@@ -1,15 +1,19 @@
 import "./Employers.scss"
 import { Container, TextField, Typography } from "@material-ui/core"
 import { useState, useEffect } from "react"
+import Stack from '@mui/material/Stack'
+import EmployerCard from "../../components/EmployerCard/EmployerCard"
 
 const Employers = () => {
+  const [employers, setEmployers] = useState([])
 
+  console.log(employers)
   useEffect(() => {
     const fetchEmployers = async () => {
       try {
         let employers = await fetch(`http://localhost:9000/employer/all`)
         let data = await employers.json()
-        console.log(data)
+        setEmployers(data)
       } catch (error) {
         console.log(error);
       }
@@ -30,6 +34,13 @@ const Employers = () => {
         <Container fixed className="search__container">
           <TextField id="standard-basic" label="Search Employers" variant="standard" />
         </Container>
+      
+        {employers[0]?.map((employer) => {
+             return <Stack className="employer__container">
+             <EmployerCard />
+                </Stack>
+          })}
+    
       </div>
     </>
   )
