fbaf78362414bc3e32d128fe21f27a0ab7bc52ac
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-06-25 20:29:58 +0100

    Added DevCard component to display developers

---
+++ devspace/src/components/DevCard/DevCard.jsx
@@ -0,0 +1,11 @@
+import "./DevCard.scss";
+import { useState, useEffect } from "react";
+import Paper from "@mui/material/Paper";
+
+const DevCard = (props) => {
+  console.log(props);
+
+  return <Paper elevation={0} />;
+};
+
+export default DevCard;
--- devspace/src/pages/Developers/Developers.js
+++ devspace/src/pages/Developers/Developers.js
@@ -1,8 +1,31 @@
 import "./Developers.scss";
 import { Container, TextField, Typography } from "@material-ui/core";
-
+import { useState, useEffect } from "react";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import DevCard from "../../components/DevCard/DevCard";
 
 const Developers = () => {
+  const [developers, setDevelopers] = useState([]);
+
+  useEffect(() => {
+    const fetchDevelopers = async () => {
+      try {
+        const allDevelopers = await fetch(
+          `http://localhost:9000/developer/all`
+        );
+        const data = await allDevelopers.json();
+        setDevelopers(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchDevelopers();
+  }, []);
+
   return (
     <>
       <div className="developer__section">
@@ -14,8 +37,25 @@ const Developers = () => {
           Discover developers.
         </Typography>
         <Container fixed className="search__container">
-          <TextField id="standard-basic" label="Search Developers" variant="standard" />
+          <TextField
+            id="standard-basic"
+            label="Search Developers"
+            variant="standard"
+          />
         </Container>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            {Array.from(Array(6)).map((_, index) => (
+              <Grid item xs={2} sm={4} md={4} key={index}>
+                <DevCard />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
       </div>
     </>
   );
